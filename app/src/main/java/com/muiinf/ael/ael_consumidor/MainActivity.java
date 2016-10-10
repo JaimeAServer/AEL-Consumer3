58bedb5e6bc135b6ecb0f6b4805d6fd61cd4b50f
Marc Forment <foments@gmail.com>
2016-10-10 17:48:19 +0200

    Initial commit

---
+++ app/src/main/java/com/muiinf/ael/ael_consumidor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.muiinf.ael.ael_consumidor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
